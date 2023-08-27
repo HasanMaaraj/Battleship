a189f0ac1a006e95540de21dbc7f532c0a2d3408
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-08-27 10:59:30 +0300

    Write shipFactory function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+
+const shipFactory = (length) => {
+    return {
+        length,
+        hits:0,
+        hit() {
+            hits++
+        },
+        isSunk() {
+            return hits >= length;
+        }
+
+    }
+}
