f160b8a77d8678662c1b2a8044fdf742227387c9
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-09-01 20:03:26 +0300

    Write gameboardFactory function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import './style.css';
 
-const shipFactory = (length) => {
+const shipFactory = length => {
     return {
         length,
         hits:0,
         hit() {
-            hits++
+            this.hits++
         },
         isSunk() {
             return hits >= length;
         }
 
     }
+}
+
+const gameboardFactory = () => {
+    let board = [
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+    ]
+
+    const placeShip = (ship, coordinates) => {
+        for (let coordinate in coordinates) {
+            if (board[coordinates[coordinate][0]][coordinates[coordinate][1]]) return false;
+        }
+        for (let coordinate in coordinates) {
+            board[coordinates[coordinate][0]][coordinates[coordinate][1]] = ship;
+        }
+
+    }
+
+    placeShip(ship, [[0,0],[0,1]])
+    return {board}
 }
