11b1b715928b15066491774da419c33b4683a3a0
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-09-02 16:25:31 +0300

    write isDefeated function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.css';
+// import './style.css';
 
 const shipFactory = length => {
     return {
@@ -29,16 +29,33 @@ const gameboardFactory = () => {
         [null, null, null, null, null, null, null, null, null, null],
     ]
 
+    let attacks = [];
+    let ships = [];
     const placeShip = (ship, coordinates) => {
         for (let coordinate in coordinates) {
             if (board[coordinates[coordinate][0]][coordinates[coordinate][1]]) return false;
         }
+        ships.push(ship);
         for (let coordinate in coordinates) {
             board[coordinates[coordinate][0]][coordinates[coordinate][1]] = ship;
         }
 
     }
+    
+    const receiveAttack = coordinates => {
+        if (attacks.includes(coordinates)) return;
+        attacks.push(coordinates)
+        if (board[coordinates[0]][coordinates[1]]) {
+            board[coordinates[0]][coordinates[1]].hit();
+        }
+    }
 
-    placeShip(ship, [[0,0],[0,1]])
+    const isDefeated = () => {
+        for (let i=0; i < ships.length; i++) {
+            if (!ships[i].isSunk) return false;
+        }
+        return true;
+    }
+
     return {board}
 }
